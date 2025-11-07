5db4e6cdc1f749fa99c23a78fa67067a4c0736f3
Hisham Ali <thehishamali@gmail.com>
2025-11-07 16:25:05 +0530

    Time: 62 ms (35.34%), Space: 56.5 MB (34.26%) - LeetHub

---
+++ 2741-function-composition/2741-function-composition.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    return function(x) {
+        let value = x
+
+        for (let i = functions.length - 1; i >= 0; i --) {
+            value = functions[i](value)
+        }
+
+        return value
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
