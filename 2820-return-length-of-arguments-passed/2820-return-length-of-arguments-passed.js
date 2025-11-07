880cdeae21d715fdbf903a2c4cc0f867d738d92f
Hisham Ali <thehishamali@gmail.com>
2025-11-07 16:26:59 +0530

    Time: 46 ms (53.64%), Space: 54.1 MB (55.86%) - LeetHub

---
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -0,0 +1,13 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    let arguments = [...args]
+
+    return arguments.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
