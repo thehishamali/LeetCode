e7368a0033b88de064b8e2a179cf4a44bd6d6865
Hisham Ali <thehishamali@gmail.com>
2025-11-07 12:00:41 +0530

    Time: 45 ms (64.75%), Space: 54 MB (50.09%) - LeetHub

---
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: (toBeVal) => {
+            if (val === toBeVal) {
+                return true
+            }
+            else {
+                throw new Error('Not Equal')
+            }
+        },
+        notToBe: (notToBeValue) => {
+            if (val !== notToBeValue) {
+                return true
+            }
+            else {
+                throw new Error('Equal')
+            }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
